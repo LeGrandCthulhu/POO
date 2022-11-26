396625ac65aa6431b45501b2f4266ff428142c16
LeGrandCthulhu <pierre.frebet@gmail.com>
2022-11-26 22:13:15 +0100

    Importation des fichiers

---
+++ test.py
@@ -0,0 +1,10 @@
+class Personne:
+    def __init__(self, nom, age) -> None:
+        self.nom = nom
+        self.age = age
+    
+    def se_presenter(self):
+        print(f"Bonjour, je m'appelle {self.nom} et j'ai {self.age} ans.")
+
+humain1 = Personne("Pierre", 16)
+humain1.se_presenter()
